2c158d12ed974a13dc29961e5ec0ca4b18cc4ca8
Hridoy Ahmed Shajib <hridoyahmed562@gmail.com>
2022-08-09 07:41:44 +0600

    Add files via upload

---
+++ Lab 04/236A_Boy or Girl.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    int l=s.length();
+    int ans=0;
+    for(int i=1; i<l; i++)
+    {
+        if(s[i]!=s[i-1])
+            ans++;
+    }
+
+    if(ans&1)
+        cout<<"CHAT WITH HER!"<<endl;
+    else
+        cout<<"IGNORE HIM!"<<endl;
+
+    return 0;
+}
---
+++ Lab 04/281A_Word Capitalization.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    s[0] = towupper(s[0]);
+    cout << s << endl;
+    return 0;
+}
